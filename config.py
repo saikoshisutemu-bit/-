d3930a3c1f43a196e6e70d8a4226e1e1a31e3932
saikoshisutemu-bit <saikoshisutemu@gmail.com>
2026-09-13 13:53:16 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+# ⚙️ ⌬⌥ 𝚂𝙰𝙸𝙺𝙾✦𝚂𝚈𝚂𝚃𝙴𝙼𝙴⬢
+# CONFIGURATION DU JEU
+
+NOM_DU_SYSTEME = "⌬⌥ 𝚂𝙰𝙸𝙺𝙾✦𝚂𝚈𝚂𝚃𝙴𝙼𝙴⬢"
+
+INCARNATION = "HIGH-RISE INVASION"
+
+# ❤️ Points de vie
+PV_MAX = 100
+
+# ⚔️ Combat
+DEGATS_BALLON = 10
+
+# ⏱️ Temps pour esquiver
+TEMPS_ESQUIVE = 5
+
+# 🩸 Blessure
+MULTIPLICATEUR_BLESSURE = 2
+
+# 🎮 Commandes
+COMMANDE_ATTAQUE = "!ball"
+COMMANDE_ESQUIVE = "!esquiv"
+COMMANDE_STATS = "!stats"
